ffa44669179cfbfaac384209b4aed78ebe7793da
Jesus Arciniega <dev.jesusarc@gmail.com>
2018-10-13 04:14:32 -0600

    Limpiar duplicados

--- public/resultados.js
+++ public/resultados.js
@@ -33,8 +33,74 @@ $(document).ready(function(){
        $('.col_'+columna).text('V');
       }
       $('select').formSelect();
+      filtrar();
     });
   });
+
+function filtrar(){
+  var tbl = document.getElementById("tabla");
+  var numRows = tbl.rows.length;
+  var array = new Array();
+  for (var i = 1; i < numRows; i++) {
+      var ID = tbl.rows[i].id;
+      var cells = tbl.rows[i].getElementsByTagName('td');
+      array[i] = new Array();
+      for (var ic=0,it=cells.length;ic<it;ic++) {
+          var value = cells[ic].innerHTML;
+          array[i].push(value);
+      }
+      //console.log(array[i]);
+  }
+  //console.log(array)
+  
+  var dataUnique = array.reduce(function (out, item) {
+  return out.concat(out.filter(function (comp) {
+    var value = comp.toString();
+    var x = value.substr(value.indexOf(',')+1);
+    //console.log(x);
+    var y = item.toString().substr(value.indexOf(',')+1);
+      return y == x;
+    }).length ? [] : [item]);
+  }, []);
+  
+  arr_diff(array, dataUnique)
+  
+  //console.log(array, dataUnique);
+  //console.log(arr_diff(array, dataUnique));
+
+}
+
+
+function arr_diff (a1, a2) {
+
+    var a = [], diff = [];
+
+    for (var i = 0; i < a1.length; i++) {
+        a[a1[i]] = true;
+    }
+
+    for (var i = 0; i < a2.length; i++) {
+        if (a[a2[i]]) {
+            delete a[a2[i]];
+        } else {
+            a[a2[i]] = true;
+        }
+    }
+
+    for (var k in a) {
+      var x = k.substr(0,k.indexOf(','));
+      //console.log(x);
+       if(x){
+         var row = 'row_' + x;
+         //console.log(row)
+         document.getElementsByClassName(row)[0].style.display="none";
+       }
+       
+        diff.push(k);
+    }
+
+    return diff;
+}
 
 
 function combinaciones(n)
@@ -56,7 +122,7 @@ function makeTableHTML(myArray,n) {
     }
     result += '</thead><tbody >';
     for(var i=0; i<myArray.length; i++) {
-        result += "<tr>";
+        result += "<tr class='row_"+(parseInt([i])+1)+"'>";
         result += "<td class='center-align' style='font-weight:bold;padding: 4px;'>"+(parseInt([i])+1)+"</td>";
         for(var j=0; j<myArray[i].length; j++){
             result += "<td class='center-align col_"+(parseInt([j])+1)+"' style='padding: 4px;'>"+myArray[i][j]+"</td>";
